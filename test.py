39562a2bbddfe0dcad473baed8324dcbc39480b6
Esteban Dulaurans <esteban.dulaurans@epita.fr>
2024-07-25 14:03:49 +0200

    feat: change axis with left click

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import vtk
 import os
+from vtk.util.numpy_support import vtk_to_numpy
+import numpy as np
 
 filePath = "Data/case6_gre1.nrrd"
 reader = vtk.vtkNrrdReader()
@@ -39,11 +41,13 @@ oblique.DeepCopy((1, 0, 0, center[0],
                   0, 0.5, 0.866025, center[2],
                   0, 0, 0, 1))
 
+matrices = [ axial, coronal, sagittal, oblique]
+
 # Extract a slice in the desired orientation
 reslice = vtk.vtkImageReslice()
 reslice.SetInputConnection(reader.GetOutputPort())
 reslice.SetOutputDimensionality(2)
-reslice.SetResliceAxes(sagittal)
+reslice.SetResliceAxes(oblique)
 reslice.SetInterpolationModeToLinear()
 
 # Create a greyscale lookup table
@@ -82,13 +86,17 @@ window.Render()
 #   buttonRepresentation->SetButtonTexture(0, image1);
 #   buttonRepresentation->SetButtonTexture(1, image2);
 
-image1 = vtk.vtkImageData()
-image1.SetDimensions(10, 10, 1)
-
-buttonRepresentation = vtk.vtkButtonRepresentation()
-buttonRepresentation.SetNumberOfStates(2)
-buttonRepresentation.SetButtonTesture(0, ...)
-buttonRepresentation.SetButtonTesture(1, ...)
+# image1 = vtk.vtkImageData()
+# image1.SetDimensions(10, 10, 1)
+# frame = vtk_to_numpy(image1.GetScalarPointer(0, 0, 0))
+#
+# image2 = vtk.vtkImageData()
+# image2.SetDimensions(10, 10, 1)
+#
+# buttonRepresentation = vtk.vtkTexturedButtonRepresentation2D()
+# buttonRepresentation.SetNumberOfStates(2)
+# buttonRepresentation.SetButtonTexture(0, image1)
+# buttonRepresentation.SetButtonTexture(1, image2)
 
 buttonActor = vtk.vtkButtonWidget()
 buttonActor.SetInteractor(interactor)
@@ -96,18 +104,27 @@ buttonActor.SetInteractor(interactor)
 # Create callbacks for slicing the image
 actions = {}
 actions["Slicing"] = 0
+actions["Axes"] = oblique
+actions["matrices_index"] = 3
+
+matrices_index = 2
 
 def ButtonCallback(obj, event):
-    if event == "LeftButtonPressEvent":
-        actions["Slicing"] = 1
-    else:
-        actions["Slicing"] = 0
+    actions["Slicing"] = event == "LeftButtonPressEvent"
+    if event == "RightButtonPressEvent":
+        actions["matrices_index"] += 1
+        actions["matrices_index"] %= 4
+        actions["Axes"] = matrices[actions["matrices_index"]]
+        reslice.SetResliceAxes(actions["Axes"])
+        reslice.Update()
+        window.Render()
 
 def MouseMoveCallback(obj, event):
     (lastX, lastY) = interactor.GetLastEventPosition()
     (mouseX, mouseY) = interactor.GetEventPosition()
     if actions["Slicing"] == 1:
         deltaY = mouseY - lastY
+        reslice.SetResliceAxes(actions["Axes"])
         reslice.Update()
         sliceSpacing = reslice.GetOutput().GetSpacing()[2]
         matrix = reslice.GetResliceAxes()
@@ -123,7 +140,9 @@ def MouseMoveCallback(obj, event):
 
 interactorStyle.AddObserver("MouseMoveEvent", MouseMoveCallback)
 interactorStyle.AddObserver("LeftButtonPressEvent", ButtonCallback)
+interactorStyle.AddObserver("RightButtonPressEvent", ButtonCallback)
 interactorStyle.AddObserver("LeftButtonReleaseEvent", ButtonCallback)
+# interactorStyle.SetInteractionModeToImage3D()
 
 # Start interaction
 interactor.Start()
