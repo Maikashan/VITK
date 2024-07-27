123e5602361da9169951d1532f11af74496fc92e
Esteban Dulaurans <esteban.dulaurans@epita.fr>
2024-07-27 19:06:11 +0200

    feat: added head as a volume

--- 3DViewTry.py
+++ 3DViewTry.py
@@ -32,6 +32,10 @@ def main():
 
     file_name = get_program_parameters()
 
+    readerHead = vtk.vtkNrrdReader()
+    readerHead.SetFileName("Data/case6_gre1.nrrd")
+    readerHead.Update()
+
     colors = vtkNamedColors()
 
     # Create the RenderWindow, Renderer and Interactor.
@@ -77,10 +81,49 @@ def main():
 
     iso_actor = vtkActor()
     iso_actor.SetMapper(iso_mapper)
-    iso_actor.GetProperty().SetColor(colors.GetColor3d('Ivory'))
+    iso_actor.GetProperty().SetColor(colors.GetColor3d('Red'))
 
     outline = vtkOutlineFilter()
     outline.SetInputConnection(reader.GetOutputPort())
+
+    opacityFun = vtk.vtkPiecewiseFunction()
+
+    # Set a mapping going from 0.0 opacity at 90, up to 0.2 at 100,
+    # and back down to 0.0 at 120.
+    opacityFun.AddPoint(0.0, 0.0)
+    opacityFun.AddPoint(90.0, 0.0)
+    opacityFun.AddPoint(100.0, 0.2)
+    opacityFun.AddPoint(120.0, 0.0)
+
+    # Create a color transfer function for the mapping of scalar
+    # value into color
+    colorFun = vtk.vtkColorTransferFunction()
+
+    # Set the color to a constant value, you might
+    # want to try (0.8, 0.4, 0.2)
+    colorFun.AddRGBPoint(0.0, .8, .4, .2)
+    colorFun.AddRGBPoint(255.0, .8, .4, .2)
+
+    # Create a volume property
+    # Set the opacity and color. Change interpolation
+    # to linear for a more pleasing image
+    property = vtk.vtkVolumeProperty()
+    property.SetScalarOpacity(opacityFun)
+    property.SetColor(colorFun)
+    property.SetInterpolationTypeToLinear()
+
+    # Create the volume mapper
+    mapper = vtk.vtkSmartVolumeMapper()
+
+    # Set the input to the output of the reader
+    mapper.SetInputConnection(readerHead.GetOutputPort())
+
+    # Create the volume
+    volume = vtk.vtkVolume()
+
+    # Set the property and the mapper
+    volume.SetProperty(property)
+    volume.SetMapper(mapper)
 
     outline_mapper = vtkPolyDataMapper()
     outline_mapper.SetInputConnection(outline.GetOutputPort())
@@ -91,6 +134,7 @@ def main():
     # Add the actors to the renderer, set the background and size.
     #
     ren.AddActor(outline_actor)
+    ren.AddActor(volume)
     ren.AddActor(iso_actor)
     ren.SetBackground(colors.GetColor3d('SlateGray'))
     ren.GetActiveCamera().SetFocalPoint(0, 0, 0)
