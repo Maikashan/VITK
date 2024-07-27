ca5bcf4c3a5229148ec3a8c126f8873c67b59505
kerian.allaire <kerian.allaire@epita.fr>
2024-07-27 21:04:14 +0200

    feat: registration implemented

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from render import render_images
-from 3DView import render_3D
+from threeDView import render_3D
 from segmentation import segmentation
 from registration import register_images
 import itk
 import vtk
 
-file_paths = ["Data/case6_gre1.nrrd",
-             "Data/case6_gre2.nrrd"]
+file_paths = ["Data/case6_gre1.nrrd", "Data/case6_gre2.nrrd"]
 
-registered_paths =
-            ["Data/case6_gre1.nrrd",
-             "Data/registered.nrrd"]
+registered_paths = ["Data/case6_gre1.nrrd", "Data/registered.nrrd"]
 
-segmented = ["segmented_1.nrrd", "segmented_2.nrrd"]
+segmented_paths = ["segmented_1.nrrd", "segmented_2.nrrd"]
 
 # REGISTRATION
 register_images(file_paths[0], file_paths[1], registered_paths[1])
@@ -20,10 +17,10 @@ register_images(file_paths[0], file_paths[1], registered_paths[1])
 
 # SEGMENTATION
 input_image = itk.imread(registered_paths[0], pixel_type=itk.D)
-segmentation(input_image, segmented[0])
+segmentation(input_image, segmented_paths[0])
 
 input_image = itk.imread(registered_paths[0], pixel_type=itk.D)
-segmentation(input_image, segmented[1])
+segmentation(input_image, segmented_paths[1])
 
 # readers = []
 # for i in range(2):
